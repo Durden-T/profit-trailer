3e3f97a9bb7a3f2f00a287ecc58ef543b86cf055
渠超越 <quchaoyue.0@bytedance.com>
2022-05-20 22:56:09 +0800

    init
    Change-Id: I2f9f73bd34a68ff9612ffd65e49060262787cad7

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM openjdk:8-alpine
 
-ENV PT_VERSION=2.4.53
+ENV PT_VERSION=2.5.10
 ENV PT_DL=https://github.com/taniman/profit-trailer/releases/download/${PT_VERSION}/ProfitTrailer-${PT_VERSION}.zip
+ENV SPRING_PROFILES_ACTIVE=prod
 
 VOLUME /app
 EXPOSE 8081
--- run-profit-trailer.sh
+++ run-profit-trailer.sh
@@ -7,7 +7,7 @@ PT="ProfitTrailer"
 PT_DIR=${APP}/${PT}
 PT_ZIP=${BASE}/${PT}-${PT_VERSION}.zip
 PT_JAR=${PT_DIR}/${PT}.jar
-PT_START="SPRING_PROFILES_ACTIVE=1;java -Djava.net.preferIPv4Stack=true -Dsun.stdout.encoding=UTF-8 -Dio.netty.allocator.numDirectArenas=0 -Djdk.nio.maxCachedBufferSize=262144 -XX:+UseSerialGC -XX:+UseStringDeduplication -Xms64m -Xmx512m -XX:CompressedClassSpaceSize=300m -XX:MaxMetaspaceSize=128m -jar $PT_JAR"
+PT_START="java -Djava.net.preferIPv4Stack=true -Dsun.stdout.encoding=UTF-8 -XX:+UseSerialGC -XX:+UseStringDeduplication -Xms64m -Xmx384m -XX:CompressedClassSpaceSize=300m -XX:MaxMetaspaceSize=256m -jar $PT_JAR"
 
 [ -d "$PT_DIR" ] || mkdir -p "$PT_DIR" || {
    echo "Error: no $PT_DIR found and could not make it. Exiting."; exit -1;
@@ -19,7 +19,7 @@ cd ${PT_DIR} || {
   echo "Error: problem with $PT_DIR. Exiting."; exit -1;
 }
 
-pcnt=$(/bin/ls -1 ${PT_DIR}/*.properties 2>/dev/null|/usr/bin/wc -l)
+pcnt=$(/bin/ls -1 ${PT_DIR}/*.jar 2>/dev/null|/usr/bin/wc -l)
 [[ ${pcnt} -gt 0 ]] || {
   echo "No properties found, extracting..."; unzip -jo ${PT_ZIP} -d ${PT_DIR};
 }
